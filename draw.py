decd45e98e33e6900964f2e61280a27b1e77dda7
dimayuga-adrieljoseph <dimayuga.adrieljoseph03@gmail.com>
2025-06-29 15:03:39 +0800

    add learning materials

--- draw.py
+++ draw.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy
 
-# image = cv.imread('Images/pic4.jpg')
-# cv.imshow('pic4', image)
+image = cv.imread('Images/pic4.jpg')
+cv.imshow('pic4', image)
 
 # displays black image
 # height and width is 500 with 3 color channel
@@ -14,9 +14,11 @@ blank_image = numpy.zeros((500, 500, 3), dtype = 'uint8')
 # blank_image[:] = 255, 255, 255
 
 #adding color to a specific area only
-blank_image[200:300] = 255, 255, 255
+# blank_image[200:300, 200:300] = 255, 255, 255
+# cv.imshow('color', blank_image)
 
-cv.imshow('color', blank_image)
-
+#draw a rectangle
+cv.rectangle(image, (1000,1500), (1500,1000), (255,0,0), 10 )
+cv.imshow('rectangle', image)
 
 cv.waitKey(0)
